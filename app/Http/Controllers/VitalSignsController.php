4780d5ce56784fafc682091781cb21d52a6f70cf
George Muchiri <george@crystalgategrp.com>
2019-04-29 15:23:24 +0300

    Update on Vitals Model and Controller

--- app/Http/Controllers/VitalSignsController.php
+++ app/Http/Controllers/VitalSignsController.php
@@ -2,10 +2,23 @@
 namespace App\Http\Controllers;
 use App\Models\VitalSigns;
 use Illuminate\Http\Request;
+use DB;
 class VitalSignsController extends Controller
 {
     public function index(Request $request)
     {
+
+
+ if ($request->query('search')) {
+            $search = $request->query('search');
+            $VitalSigns = VitalSigns::with('patient.name')->where('description', 'LIKE', "%{$search}%")
+                ->paginate(10);
+        } else {
+            $VitalSigns = VitalSigns::with('patient.name')->orderBy('id', 'ASC')->paginate(10);
+        }
+
+        return response()->json($VitalSigns);
+
     }
     /**
      * Store a newly created resource in storage.
@@ -13,23 +26,27 @@ class VitalSignsController extends Controller
      * @param  \Illuminate\Http\Request
      * @return \Illuminate\Http\Response
      */
+
     public function store(Request $request)
     {
         $rules = [
-            'body_temperature' => 'required',
-            'respiratory_rate' => 'required',
-            'heart_rate' => 'required',
-            'blood_pressure' => 'required',
+           
         ];
         $validator = \Validator::make($request->all(), $rules);
         if ($validator->fails()) {
             return response()->json($validator, 422);
         } else {
             $VitalSigns = new VitalSigns;
+           $VitalSigns->patient_id = $request->input('patient_id');
+
             $VitalSigns->body_temperature = $request->input('body_temperature');
             $VitalSigns->respiratory_rate = $request->input('respiratory_rate');
             $VitalSigns->heart_rate = $request->input('heart_rate');
             $VitalSigns->blood_pressure = $request->input('blood_pressure');
+            $VitalSigns->height = $request->input('height');
+            $VitalSigns->weight = $request->input('weight');
+            $VitalSigns->body_mass_index = $request->input('body_mass_index');
+            $VitalSigns->body_surface_area = $request->input('body_surface_area');
             try {
                 $VitalSigns->save();
                 return response()->json($VitalSigns);
@@ -59,20 +76,23 @@ class VitalSignsController extends Controller
     public function update(Request $request, $id)
     {
         $rules = [
-              'body_temperature' => 'required',
-            'respiratory_rate' => 'required',
-            'heart_rate' => 'required',
-            'blood_pressure' => 'required',
-        ];
+    
+    ];
         $validator = \Validator::make($request->all(), $rules);
         if ($validator->fails()) {
             return response()->json($validator, 422);
         } else {
             $VitalSigns = VitalSigns::findOrFail($id);
+            $VitalSigns->patient_id = $request->input('patient_id');
             $VitalSigns->body_temperature = $request->input('body_temperature');
             $VitalSigns->respiratory_rate = $request->input('respiratory_rate');
             $VitalSigns->heart_rate = $request->input('heart_rate');
             $VitalSigns->blood_pressure = $request->input('blood_pressure');
+
+            $VitalSigns->height = $request->input('height');
+            $VitalSigns->weight = $request->input('weight');
+            $VitalSigns->body_mass_index = $request->input('body_mass_index');
+            $VitalSigns->body_surface_area = $request->input('body_surface_area');
             try {
                 $VitalSigns->save();
                 return response()->json($VitalSigns);
